b7344bc8c158aa7d7b6ce6fa72e321eaf5f4192e
Fabio <fabio.waljaard@gmail.com>
2024-04-27 14:50:38 +0000

    Improve config flow

--- homeassistant/components/youtube_lounge/config_flow.py
+++ homeassistant/components/youtube_lounge/config_flow.py
@@ -4,6 +4,8 @@ from __future__ import annotations
 import logging
 from typing import Any
 
+from aiogoogle import Aiogoogle
+from aiogoogle.excs import HTTPError
 from pyytlounge import YtLoungeApi
 import voluptuous as vol
 
@@ -17,18 +19,52 @@ from .util import device_name
 
 _LOGGER = logging.getLogger(__name__)
 
-STEP_USER_DATA_SCHEMA = vol.Schema(
+STEP_GOOGLE_API_DATA_SCHEMA = vol.Schema(
     {
-        vol.Required("pairing_code", description="pairing_code"): str,
         vol.Optional("google_api_key", description="google_api_key"): str,
     }
 )
 
+STEP_PAIR_DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required("pairing_code", description="pairing_code"): str,
+    }
+)
 
-async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
+
+async def validate_google_api_key(
+    hass: HomeAssistant, data: dict[str, Any]
+) -> dict[str, Any]:
     """Validate the user input allows us to connect.
 
-    Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
+    Data has the keys from STEP_GOOGLE_API_DATA_SCHEMA with values provided by the user.
+    """
+
+    api_key = data["google_api_key"]
+    # supplying a key is optional
+    if api_key:
+        async with Aiogoogle(api_key=api_key) as aiogoogle:
+            yt_api = await aiogoogle.discover("youtube", "v3")
+            request = yt_api.videos.list(part="snippet", id="oa__fLArsFk")
+            try:
+                await aiogoogle.as_api_key(request)
+            except HTTPError as ex:
+                if ex.res and ex.res.status_code == 400:
+                    raise InvalidAuth from ex
+                else:
+                    raise CannotConnect from ex
+            except Exception as ex:
+                raise CannotConnect from ex
+
+    return {}
+
+
+async def validate_pairing_code(
+    hass: HomeAssistant, data: dict[str, Any]
+) -> dict[str, Any]:
+    """Validate the user input allows us to connect.
+
+    Data has the keys from STEP_PAIR_DATA_SCHEMA with values provided by the user.
     """
 
     code: int
@@ -55,16 +91,43 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
-        """Handle the initial step."""
+        """Handle the initial step to set the google api key."""
         if user_input is None:
             return self.async_show_form(
-                step_id="user", data_schema=STEP_USER_DATA_SCHEMA
+                step_id="user", data_schema=STEP_GOOGLE_API_DATA_SCHEMA
             )
 
         errors = {}
 
         try:
-            info = await validate_input(self.hass, user_input)
+            await validate_google_api_key(self.hass, user_input)
+        except CannotConnect:
+            errors["base"] = "cannot_connect"
+        except InvalidAuth:
+            errors["base"] = "invalid_auth"
+        except Exception:  # pylint: disable=broad-except
+            _LOGGER.exception("Unexpected exception")
+            errors["base"] = "unknown"
+        else:
+            return await self.async_step_pair(user_input)
+
+        return self.async_show_form(
+            step_id="user", data_schema=STEP_GOOGLE_API_DATA_SCHEMA, errors=errors
+        )
+
+    async def async_step_pair(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle the pairing step."""
+        if user_input is None or "pairing_code" not in user_input:
+            return self.async_show_form(
+                step_id="pair", data_schema=STEP_PAIR_DATA_SCHEMA
+            )
+
+        errors = {}
+
+        try:
+            info = await validate_pairing_code(self.hass, user_input)
         except CannotConnect:
             errors["base"] = "cannot_connect"
         except InvalidAuth:
@@ -76,7 +139,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             return self.async_create_entry(title=info["title"], data=user_input)
 
         return self.async_show_form(
-            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+            step_id="pair", data_schema=STEP_PAIR_DATA_SCHEMA, errors=errors
         )
 
 
